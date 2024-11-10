3dd19f6fc9035dde554391510d80112d88137d85
wardru <agouby@panduza.io>
2024-11-10 22:02:22 +0100

    Messaging for async client connections working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,62 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+pub mod mqtt_handler;
+
+use tauri::{Manager, State};
+use mqtt_handler::{MqttHandler, MqttMessageType};
+use std::time::Duration;
+use tokio::sync::Mutex;
+use rumqttc::ConnectionError;
+
+#[tauri::command]
+async fn connect(mqtt_handler_state: State<'_, Mutex<MqttHandler>>) -> Result<(), String> {
+
+  let mut handler = mqtt_handler_state.lock().await;
+
+  let mut rec = handler.connect("localhost", 1883).await.map_err(|e| {println!("Connection error {:?}", e.to_string()); e.to_string()})?;
+
+  tokio::spawn(async move {
+    while let Some(msg) = rec.recv().await {
+      println!("msg: {msg:?}");
+    }
+  });
+
+  let mdr = handler.get_message_provider();
+
+  mdr.send(MqttMessageType::Subscribe("pza/_/structure/att".into())).await;
+
+  Ok(())
+}
+
+#[tauri::command]
+async fn disconnect(mqtt_handler_state: State<'_, Mutex<MqttHandler>>) -> Result<(), ()> {
+  let mut handler = mqtt_handler_state.lock().await;
+
+  handler.disconnect().await;
+  Ok(())
+}
 
 #[tokio::main]
 async fn main() {
-  app_lib::run();
+
+  let handler = MqttHandler::new();
+
+   tauri::Builder::default()
+    .setup(|app| {
+      app.manage(Mutex::new(handler));
+      if cfg!(debug_assertions) {
+        app.handle().plugin(
+          tauri_plugin_log::Builder::default()
+            .level(log::LevelFilter::Info)
+            .build(),
+        )?;
+      }
+      Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![
+      connect,
+      disconnect
+    ])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
--- src-tauri/src/mqtt_handler.rs
+++ src-tauri/src/mqtt_handler.rs
@@ -1,111 +1,141 @@
-use rumqttc::{AsyncClient, Event, EventLoop, Incoming, MqttOptions};
+use rumqttc::{AsyncClient, ClientError, ConnectionError, Event, EventLoop, Incoming, MqttOptions, QoS};
+use core::str;
 use std::time::Duration;
-use tokio::{sync::mpsc, task, time, time::timeout};
+use tokio::{sync::{mpsc, watch}, task, time::{self, error::{self, Elapsed}, timeout}};
+use uuid::Uuid;
+use std::collections::HashMap;
+use tokio::sync::Mutex;
+use std::sync::Arc;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum ConnState {
-    Connected,
-    Disconnected,
-}
+type MqttMessage = (String, String);
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum Message {
-    NewState(ConnState),
-    Publish(String)
-}
-
-pub struct MqttHandlerBuilder {
-    connection_timeout: std::time::Duration,
-}
-
-impl MqttHandlerBuilder {
-    pub fn new() -> MqttHandlerBuilder {
-        Self {
-            connection_timeout: Duration::from_secs(5),
-        }
-    }
-
-    pub fn with_connection_timeout(mut self, timeout: std::time::Duration) -> MqttHandlerBuilder {
-        self.connection_timeout = timeout;
-        self
-    }
-
-    pub fn build(&self) -> MqttHandler {
-        MqttHandler {
-            connection_state: ConnState::Disconnected,
-            rumqtt_client: None,
-            connection_timeout: self.connection_timeout,
-            receiver: None 
-        }
-    }
+#[derive(Debug)]
+pub enum MqttMessageType
+{
+    Publish(MqttMessage),
+    Subscribe(String),
+    Unsubscribe(String)
 }
 
 pub struct MqttHandler {
-    connection_state: ConnState,
-    rumqtt_client: Option<AsyncClient>,
+    rumqtt_client: Option<Arc<Mutex<AsyncClient>>>,
     connection_timeout: std::time::Duration,
-    receiver: Option<mpsc::Receiver<Message>>
+
+    message_provider_tx: mpsc::Sender<MqttMessageType>,
+    message_provider_rx: mpsc::Receiver<MqttMessageType>,
+    
+    event_loop_handle: Option<task::JoinHandle<()>>,
+    message_loop_handle: Option<task::JoinHandle<()>>,
+    disconnect_handle: Option<task::JoinHandle<()>>
 }
 
 impl MqttHandler {
+
+    pub fn new() -> Self {
+
+        let (provider_tx, provider_rx) = mpsc::channel(42);
+
+        Self {
+            rumqtt_client: None,
+            connection_timeout: Duration::from_secs(3),
+
+            message_provider_rx: provider_rx,
+            message_provider_tx: provider_tx,
+
+            event_loop_handle: None,
+            message_loop_handle: None,
+            disconnect_handle: None
+        }
+    }
+
     pub async fn connect<T>(
         &mut self,
         address: T,
         port: u16,
-        id: T,
-    ) -> Result<(), rumqttc::ConnectionError>
+    ) -> Result<mpsc::Receiver<MqttMessage>, ConnectionError>
     where
         T: Into<String>,
     {
-        let mqtt_options = MqttOptions::new(id, address, port);
-        let (client, mut event_loop) = AsyncClient::new(mqtt_options, 100);
-        let (tx, mut rx) = mpsc::channel(100);
-        let _event_loop_handle = task::spawn(async move {
-            loop {
-                let event = event_loop.poll().await;
+        let mut mqtt_options = MqttOptions::new(Uuid::new_v4(), address, port);
+        mqtt_options.set_keep_alive(Duration::from_secs(5));
+        let (client, eventloop) = AsyncClient::new(mqtt_options, 100);
 
-                match event {
-                    Ok(Event::Incoming(Incoming::ConnAck(v))) => {
-                        println!("hello {v:?}");
-                        let _ = tx.send(Message::NewState(ConnState::Connected)).await;
-                    }
-                    Ok(v) => {
-                        println!("hoy {v:?}");
-                        let _ = tx.send(Message::NewState(ConnState::Disconnected));
-                    }
-                    Err(e) => {
-                        let _ = tx.send(Message::NewState(ConnState::Disconnected));
-                        println!("mdr {e:?}");
-                        time::sleep(Duration::from_secs(1)).await;
-                    }
-                }
-            }
-        });
+        self.rumqtt_client = Some(Arc::new(Mutex::new(client)));
+
+        let (message_provider_tx, message_provider_rx) = mpsc::channel(42);
+        let (message_dispatcher_tx, message_dispatcher_rx) = mpsc::channel(42);
+        let (connection_state_tx, mut connection_state_rx) = mpsc::channel(10);
+        
+        self.message_provider_tx = message_provider_tx;
+
+        self.event_loop_handle = Some(task::spawn(Self::run_event_loop(eventloop, connection_state_tx, message_dispatcher_tx)));
+        self.message_loop_handle = Some(task::spawn(Self::run_message_loop(self.rumqtt_client.as_ref().unwrap().clone(), message_provider_rx)));
 
         let result = timeout(self.connection_timeout, async {
-            while let Some(message) = rx.recv().await {
-                if message == Message::NewState(ConnState::Connected) {
-                    return Ok(message);
+            loop {
+                match connection_state_rx.recv().await {
+                    Some(Ok(msg)) if msg == "Connected" => return Ok(()),
+                    Some(Err(e)) => return Err(e),
+                    _ => {}
                 }
             }
-            Err("efe")
-        }).await;
+        }).await.map_err(|e| ConnectionError::NetworkTimeout)??;
 
-        println!("res: {result:?}");
-
-        self.rumqtt_client = Some(client);
-        self.receiver = Some(rx);
-
-        Ok(())
+        Ok((message_dispatcher_rx))
     }
 
-    pub async fn disconnect(&self) -> Result<(), rumqttc::ClientError> {
+    async fn run_message_loop(mut client: Arc<Mutex<AsyncClient>>, mut message_receiver: mpsc::Receiver<MqttMessageType>) {
+        while let Some(msg) = message_receiver.recv().await {
+            println!("Wooh we got an order! {:?}", msg);
+            let client = client.lock().await;
+
+            match msg {
+                MqttMessageType::Publish(m) => {
+                    client.publish(m.0, QoS::AtLeastOnce, false, m.1).await;
+                }
+                MqttMessageType::Subscribe(t) => {
+                    client.subscribe(t, QoS::AtLeastOnce).await;
+                }
+                MqttMessageType::Unsubscribe(t) => {
+                    client.unsubscribe(t).await;
+                }
+            }
+        }
+    }
+
+    async fn run_event_loop(mut eventloop: EventLoop, state_tx: mpsc::Sender<Result<String, ConnectionError>>, dispatcher_tx: mpsc::Sender<MqttMessage>) {
+        loop {
+            let event = eventloop.poll().await;
+            
+            match event {
+                Ok(Event::Incoming(Incoming::ConnAck(p))) => {
+                    state_tx.send(Ok("Connected".into())).await;
+                }
+                Ok(Event::Incoming(Incoming::Disconnect)) => {
+                    state_tx.send(Ok("Disconnected".into())).await;
+                }
+                Ok(Event::Incoming(Incoming::Publish(m))) => {
+                    dispatcher_tx.send((m.topic, str::from_utf8(&m.payload).clone().unwrap().to_string())).await;
+                }
+                Err(e) => {
+                    state_tx.send(Err(e)).await;
+                    return ;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    pub async fn disconnect(&self) {
         if let Some(client) = &self.rumqtt_client {
-            client.disconnect().await?
+            client.lock().await.disconnect().await;
         }
         else {
-            println!("Called disconnect on not connected client.");
+            println!("Can't disconnect, client is not connected");
         }
-        Ok(())
+    }
+
+    pub fn get_message_provider(&self) -> mpsc::Sender<MqttMessageType> {
+        self.message_provider_tx.clone()
     }
 }
