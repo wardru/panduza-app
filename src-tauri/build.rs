388ee4cbac49823b317557a6b1a897e84cf97c3f
Edmundo Ferreira <fc.edmundo@gmail.com>
2024-12-15 21:50:34 +0100

    build: Stop removing first character of the git describe to avoid `v` on tags
    - adopted tags without `v` at the start

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -4,7 +4,7 @@ fn main() {
     let git_describe = Command::new("git")
         .args(["describe", "--tags", "--dirty"])
         .output()
-        .map(|o| String::from_utf8_lossy(&o.stdout).trim()[1..].to_string())
+        .map(|o| String::from_utf8_lossy(&o.stdout).trim().to_string())
         .unwrap_or_else(|_| "unknown".to_string());
 
     println!("cargo:rustc-env=GIT_DESCRIBE={}", git_describe);
