c14120479b50ece2a2e6a371ac3db1e537492238
Antoine Gouby <agouby@panduza.io>
2025-02-01 15:07:04 +0100

    Disable default events on copy, paste and cut to avoid alerts

--- src/app/AppEvents.ts
+++ src/app/AppEvents.ts
@@ -11,6 +11,27 @@ export const useAppEvents = (props: useAppEventsProps) => {
 
     useEffect(() => {
         let unlisten: (() => void) | undefined;
+        //disable default window behaviors on some events to avoid alert sounds
+        const events = ['cut', 'copy', 'paste'];
+
+        const preventDefaultWindowEvents = (event: Event) => {
+            const composedPath = event.composedPath()[0];
+
+            if (
+                !(
+                    composedPath instanceof HTMLInputElement ||
+                    composedPath instanceof HTMLAreaElement ||
+                    composedPath instanceof HTMLTextAreaElement
+                )
+            ) {
+                event.preventDefault();
+                // preventDefault() if it is not an input item
+            }
+        };
+
+        for (const event of events) {
+            document.addEventListener(event, preventDefaultWindowEvents);
+        }
 
         const handleKeyDown = async (event: KeyboardEvent) => {
             if (event.key == 's' && event.ctrlKey) {
@@ -35,6 +56,11 @@ export const useAppEvents = (props: useAppEventsProps) => {
 
         return () => {
             document.removeEventListener('keydown', handleKeyDown);
+
+            for (const event of events) {
+                document.removeEventListener(event, preventDefaultWindowEvents);
+            }
+
             if (unlisten) unlisten();
         };
     }, [onSaveKey, onAppClose]);
