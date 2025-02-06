d1adec192dff107a7b453adae6ac5897618389a7
Edmundo Ferreira <fc.edmundo@gmail.com>
2025-02-06 09:03:23 +0100

    cargo: Fix git describe in cargo incremental builds

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -7,6 +7,8 @@ fn main() {
         .map(|o| String::from_utf8_lossy(&o.stdout).trim().to_string())
         .unwrap_or_else(|_| "unknown".to_string());
 
+    println!("cargo:rerun-if-changed=../.git/HEAD");
+    println!("cargo:rerun-if-changed=.git/index"); // Track staging changes
     println!("cargo:rustc-env=GIT_DESCRIBE={}", git_describe);
 
     tauri_build::build();
